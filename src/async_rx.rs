zach-schoenberger/crossfire-rs

commit fe696e3c6cd2599db221fed35060654c6c345b6e

async: Make sure no old waker left for safety
Due to code-review for https://github.com/frostyplanet/crossfire-rs/issues/14

src/async_rx.rs
@@ -170,26 +170,12 @@ impl<T> AsyncRx<T> {
         // make sure always take the o_waker out and abandon,
         // to skip the timeout cleaning logic in Drop.
         let r = self.try_recv();
+        if let Some(old_waker) = o_waker.take() {
+            // https://github.com/frostyplanet/crossfire-rs/issues/14
+            old_waker.cancel();
+        }
         if let Err(TryRecvError::Empty) = &r {
-            if let Some(old_waker) = o_waker.as_ref() {
-                if old_waker.is_waked() {
-                    let _ = o_waker.take(); // should reg again
-                } else {
-                    if self.shared.get_tx_count() == 0 {
-                        if self.recv.is_empty() {
-                            // Check channel close before sleep
-                            return Err(TryRecvError::Disconnected);
-                        }
-                    } else {
-                        // False wake up, sleep again
-                        return Err(TryRecvError::Empty);
-                    }
-                }
-            }
         } else {
-            if let Some(old_waker) = o_waker.take() {
-                self.shared.cancel_recv_waker(old_waker);
-            }
             return r;
         }
         let waker = self.shared.reg_recv_async(ctx);

src/async_tx.rs
@@ -133,25 +133,16 @@ impl<T: Unpin + Send + 'static> AsyncTx<T> {
         // When the result is not TrySendError::Full,
         // make sure always take the o_waker out and abandon,
         // to skip the timeout cleaning logic in Drop.
+        //
+        // crossbeam-channel will check disconnected for us (if not raced)
         let r = self.try_send(item);
+        if let Some(old_waker) = o_waker.take() {
+            // https://github.com/frostyplanet/crossfire-rs/issues/14
+            old_waker.cancel();
+        }
         if let Err(TrySendError::Full(t)) = r {
-            if self.shared.get_rx_count() == 0 {
-                // Check channel close before sleep
-                return Err(TrySendError::Disconnected(t));
-            }
-            if let Some(old_waker) = o_waker.as_ref() {
-                if old_waker.is_waked() {
-                    let _ = o_waker.take(); // reg again
-                } else {
-                    // False wakeup
-                    return Err(TrySendError::Full(t));
-                }
-            }
             item = t;
         } else {
-            if let Some(old_waker) = o_waker.take() {
-                self.shared.cancel_send_waker(old_waker);
-            }
             return r;
         }
         let waker = self.shared.reg_send_async(ctx);

commit 2bc93927186cb5868ad36e5f4cd5257593a38ac7

Fix too many wakers when select! with no message to recv
Add assert in test_basic_unbounded_idle_select.

Previously only produce waker during select, but will clear them once
ReceiveFuture is dropped, so this problem is minor. Only effect when
select! code that never finish.

It's a regression introduced by

    commit fe696e3c6cd2599db221fed35060654c6c345b6e
    Author: plan <frostyplanet@gmail.com>
    Date:   Wed Jul 16 15:57:28 2025 +0800

	async: Make sure no old waker left for safety

	Due to code-review for https://github.com/frostyplanet/crossfire-rs/issues/14

src/async_rx.rs
@@ -191,34 +191,37 @@ impl<T> AsyncRx<T> {
         let limit = if self.bound_size == 1 { BACKOFF_LIMIT } else { 1 };
         for _ in 0..limit {
             let r = self.try_recv();
-            if let Some(old_waker) = o_waker.take() {
-                // https://github.com/frostyplanet/crossfire-rs/issues/14
-                old_waker.cancel();
-            }
             if let Err(TryRecvError::Empty) = &r {
             } else {
+                let _ = o_waker.take();
                 return r;
             }
             backoff.snooze();
         }
-        let waker = self.shared.reg_recv_async(ctx);
-        // NOTE: The other side put something whie reg_send and did not see the waker,
-        // should check the channel again, otherwise might incur a dead lock.
-        let r = self.try_recv();
-        if let Err(TryRecvError::Empty) = &r {
-            waker.commit();
-            // Check channel close before sleep, otherwise might block forever
-            // Confirmed by test_pressure_1_tx_blocking_1_rx_async()
-            if self.shared.is_disconnected() {
-                // Ensure all message is received.
-                if let Ok(msg) = self.try_recv() {
-                    return Ok(msg);
+        if self.shared.reg_recv_async(ctx, o_waker) {
+            // NOTE: The other side put something whie reg_send and did not see the waker,
+            // should check the channel again, otherwise might incur a dead lock.
+            let r = self.try_recv();
+            if let Err(TryRecvError::Empty) = &r {
+                if let Some(waker) = o_waker.as_ref() {
+                    waker.commit();
                 }
-                return Err(TryRecvError::Disconnected);
+            } else {
+                let _ = o_waker.take();
+                return r;
             }
-            o_waker.replace(waker);
         }
-        return r;
+        // Check channel close before sleep, otherwise might block forever
+        // Confirmed by test_pressure_1_tx_blocking_1_rx_async()
+        if self.shared.is_disconnected() {
+            let _ = o_waker.take();
+            // Ensure all message is received.
+            if let Ok(msg) = self.try_recv() {
+                return Ok(msg);
+            }
+            return Err(TryRecvError::Disconnected);
+        }
+        return Err(TryRecvError::Empty);
     }
 
     #[inline]

src/async_tx.rs
@@ -203,32 +203,35 @@ impl<T: Unpin + Send + 'static> AsyncTx<T> {
         let limit = if self.bound_size == 1 { BACKOFF_LIMIT } else { 1 };
         for _ in 0..limit {
             let r = self.try_send(item);
-            if let Some(old_waker) = o_waker.take() {
-                // https://github.com/frostyplanet/crossfire-rs/issues/14
-                old_waker.cancel();
-            }
             if let Err(TrySendError::Full(t)) = r {
                 item = t;
             } else {
+                let _ = o_waker.take();
                 return r;
             }
             backoff.snooze();
         }
-        let waker = self.shared.reg_send_async(ctx);
-        // NOTE: The other side put something whie reg_send and did not see the waker,
-        // should check the channel again, otherwise might incur a dead lock.
-        let r = self.try_send(item);
-        if let Err(TrySendError::Full(t)) = r {
-            if self.shared.is_disconnected() {
-                // Check channel close before sleep, otherwise might block forever
-                // Confirmed by test_pressure_1_tx_blocking_1_rx_async()
-                return Err(TrySendError::Disconnected(t));
+        if self.shared.reg_send_async(ctx, o_waker) {
+            // NOTE: The other side put something whie reg_send and did not see the waker,
+            // should check the channel again, otherwise might incur a dead lock.
+            let r = self.try_send(item);
+            if let Err(TrySendError::Full(t)) = r {
+                item = t;
+                if let Some(waker) = o_waker.as_ref() {
+                    waker.commit();
+                }
+            } else {
+                let _ = o_waker.take();
+                return r;
             }
-            waker.commit();
-            o_waker.replace(waker);
-            return Err(TrySendError::Full(t));
         }
-        return r;
+        if self.shared.is_disconnected() {
+            let _ = o_waker.take();
+            // Check channel close before sleep, otherwise might block forever
+            // Confirmed by test_pressure_1_tx_blocking_1_rx_async()
+            return Err(TrySendError::Disconnected(item));
+        }
+        return Err(TrySendError::Full(item));
     }
 
     /// Send a message while **blocking the current thread**. Be careful!

src/channel.rs
@@ -71,16 +71,66 @@ impl ChannelShared {
         }
     }
 
-    /// Register waker for current rx
+    /// Register waker for current rx, return false when it's a waker not woken up
     #[inline(always)]
-    pub(crate) fn reg_recv_async(&self, ctx: &mut Context) -> LockedWaker {
-        self.recvs.reg_async(ctx)
+    pub(crate) fn reg_recv_async(
+        &self, ctx: &mut Context, o_waker: &mut Option<LockedWaker>,
+    ) -> bool {
+        let _waker = if let Some(waker) = o_waker.as_ref() {
+            let state = waker.get_state();
+            if state == WakerState::WAITING as u8 {
+                // which is not woken, can be reuse.
+                // https://github.com/frostyplanet/crossfire-rs/issues/14
+                if waker.will_wake(ctx) {
+                    return false;
+                } else {
+                    self.recvs.cancel_waker(waker);
+                    LockedWaker::new(ctx)
+                }
+            } else if state != WakerState::WAKED as u8 {
+                // not possible
+                LockedWaker::new(ctx)
+            } else {
+                waker.reset_init();
+                o_waker.take().unwrap()
+            }
+        } else {
+            LockedWaker::new(ctx)
+        };
+        self.recvs.reg_waker(&_waker);
+        o_waker.replace(_waker);
+        true
     }
 
-    /// Register waker for current tx
+    /// Register waker for current tx, return false when it's a waker not woken up
     #[inline(always)]
-    pub(crate) fn reg_send_async(&self, ctx: &mut Context) -> LockedWaker {
-        self.senders.reg_async(ctx)
+    pub(crate) fn reg_send_async(
+        &self, ctx: &mut Context, o_waker: &mut Option<LockedWaker>,
+    ) -> bool {
+        let _waker = if let Some(waker) = o_waker.as_ref() {
+            let state = waker.get_state();
+            if state == WakerState::WAITING as u8 {
+                // which is not woken, can be reuse.
+                // https://github.com/frostyplanet/crossfire-rs/issues/14
+                if waker.will_wake(ctx) {
+                    return false;
+                } else {
+                    self.senders.cancel_waker(waker);
+                    LockedWaker::new(ctx)
+                }
+            } else if state != WakerState::WAKED as u8 {
+                // not possible
+                LockedWaker::new(ctx)
+            } else {
+                waker.reset_init();
+                o_waker.take().unwrap()
+            }
+        } else {
+            LockedWaker::new(ctx)
+        };
+        self.senders.reg_waker(&_waker);
+        o_waker.replace(_waker);
+        true
     }
 
     /// Wake up one rx

src/locked_waker.rs
@@ -1,7 +1,7 @@
 use crate::collections::WeakCell;
 use std::fmt;
 use std::sync::{
-    atomic::{AtomicU8, Ordering},
+    atomic::{AtomicU64, AtomicU8, Ordering},
     Arc, Weak,
 };
 use std::task::*;
@@ -12,7 +12,12 @@ pub struct LockedWaker(Arc<LockedWakerInner>);
 impl fmt::Debug for LockedWaker {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let _self = self.0.as_ref();
-        write!(f, "LockedWaker(seq={}, state={})", _self.seq, _self.state.load(Ordering::Acquire))
+        write!(
+            f,
+            "LockedWaker(seq={}, state={})",
+            _self.seq.load(Ordering::Acquire),
+            _self.state.load(Ordering::Acquire)
+        )
     }
 }
 
@@ -26,7 +31,7 @@ pub(crate) enum WakerState {
 struct LockedWakerInner {
     waker: std::task::Waker,
     state: AtomicU8,
-    seq: u64,
+    seq: AtomicU64,
 }
 
 pub struct LockedWakerRef(Weak<LockedWakerInner>);
@@ -39,9 +44,9 @@ impl fmt::Debug for LockedWakerRef {
 
 impl LockedWaker {
     #[inline(always)]
-    pub(crate) fn new(ctx: &Context, seq: u64) -> Self {
+    pub(crate) fn new(ctx: &Context) -> Self {
         let s = Arc::new(LockedWakerInner {
-            seq,
+            seq: AtomicU64::new(0),
             waker: ctx.waker().clone(),
             state: AtomicU8::new(WakerState::INIT as u8),
         });
@@ -50,23 +55,44 @@ impl LockedWaker {
 
     #[inline(always)]
     pub(crate) fn get_seq(&self) -> u64 {
-        self.0.seq
+        self.0.seq.load(Ordering::Acquire)
     }
 
-    // return is_already waked
     #[inline(always)]
-    pub(crate) fn cancel(&self) {
-        self.0.state.store(WakerState::WAKED as u8, Ordering::Release)
+    pub(crate) fn set_seq(&self, seq: u64) {
+        self.0.seq.store(seq, Ordering::Release);
     }
 
+    #[inline(always)]
+    pub(crate) fn reset_init(&self) {
+        self.0.state.store(WakerState::INIT as u8, Ordering::Release);
+    }
+
+    #[inline(always)]
     pub(crate) fn commit(&self) {
-        self.0.state.store(WakerState::WAITING as u8, Ordering::Release);
+        // Content with wake() on the other-side
+        let _ = self.0.state.compare_exchange(
+            WakerState::INIT as u8,
+            WakerState::WAITING as u8,
+            Ordering::SeqCst,
+            Ordering::Relaxed,
+        );
     }
 
     // return is_already waked
     #[inline(always)]
     pub(crate) fn abandon(&self) -> u8 {
         return self.0.state.swap(WakerState::WAKED as u8, Ordering::SeqCst);
+    }
+
+    #[inline(always)]
+    pub(crate) fn get_state(&self) -> u8 {
+        self.0.state.load(Ordering::Acquire)
+    }
+
+    #[inline(always)]
+    pub(crate) fn will_wake(&self, ctx: &Context) -> bool {
+        self.0.waker.will_wake(ctx.waker())
     }
 
     #[inline(always)]

src/waker_registry.rs
@@ -1,7 +1,6 @@
 use crate::collections::LockedQueue;
 use crate::locked_waker::*;
 use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
-use std::task::Context;
 
 #[enum_dispatch(RegistryTrait)]
 pub enum Registry {
@@ -13,11 +12,11 @@ pub enum Registry {
 #[enum_dispatch]
 pub trait RegistryTrait {
     /// For async context
-    fn reg_async(&self, _ctx: &mut Context) -> LockedWaker;
+    fn reg_waker(&self, waker: &LockedWaker);
 
     fn clear_wakers(&self, _seq: u64);
 
-    fn cancel_waker(&self, _waker: LockedWaker);
+    fn cancel_waker(&self, _waker: &LockedWaker);
 
     fn fire(&self);
 
@@ -39,7 +38,7 @@ impl RegistryDummy {
 
 impl RegistryTrait for RegistryDummy {
     #[inline(always)]
-    fn reg_async(&self, _ctx: &mut Context) -> LockedWaker {
+    fn reg_waker(&self, _waker: &LockedWaker) {
         unreachable!();
     }
 
@@ -47,7 +46,7 @@ impl RegistryTrait for RegistryDummy {
     fn clear_wakers(&self, _seq: u64) {}
 
     #[inline(always)]
-    fn cancel_waker(&self, _waker: LockedWaker) {}
+    fn cancel_waker(&self, _waker: &LockedWaker) {}
 
     #[inline(always)]
     fn fire(&self) {}
@@ -76,16 +75,14 @@ impl RegistrySingle {
 impl RegistryTrait for RegistrySingle {
     /// return is_skip
     #[inline(always)]
-    fn reg_async(&self, ctx: &mut Context) -> LockedWaker {
-        let waker = LockedWaker::new(ctx, 0);
+    fn reg_waker(&self, waker: &LockedWaker) {
         self.cell.put(waker.weak());
-        waker
     }
 
     #[inline(always)]
-    fn cancel_waker(&self, _waker: LockedWaker) {
+    fn cancel_waker(&self, _waker: &LockedWaker) {
         // Got to be it, because only one single thread.
-        self.cell.clear();
+        // It's ok to ignore it.
     }
 
     #[inline(always)]
@@ -138,16 +135,20 @@ impl RegistryMulti {
 
 impl RegistryTrait for RegistryMulti {
     #[inline(always)]
-    fn reg_async(&self, ctx: &mut Context) -> LockedWaker {
-        let waker = LockedWaker::new(ctx, self.seq.fetch_add(1, Ordering::SeqCst));
+    fn reg_waker(&self, waker: &LockedWaker) {
+        let seq = self.seq.fetch_add(1, Ordering::SeqCst);
+        waker.set_seq(seq);
         self.queue.push(waker.weak());
-        waker
     }
 
     #[inline(always)]
-    fn cancel_waker(&self, waker: LockedWaker) {
-        // Just abandon and leave it to fire() to clean it
-        waker.cancel();
+    fn cancel_waker(&self, waker: &LockedWaker) {
+        let seq = waker.get_seq();
+        if let Some(waker_ref) = self.queue.pop() {
+            waker_ref.try_to_clear(seq);
+            // Just abandon and leave it to fire() to clean it.
+            // At most try one.
+        }
     }
 
     /// Call when ReceiveFuture is cancelled.

src/tests/test_async.rs
@@ -308,6 +308,7 @@ fn test_basic_unbounded_1_thread<T: BlockingTxTrait<i32>, R: AsyncRxTrait<i32>>(
 fn test_basic_unbounded_idle_select<T: BlockingTxTrait<i32>, R: AsyncRxTrait<i32>>(
     setup_log: (), #[case] channel: (T, R),
 ) {
+    // This case simulate the usage of a close notification channel
     let (_tx, rx) = channel;
 
     use futures::{pin_mut, select, FutureExt};
@@ -319,8 +320,10 @@ fn test_basic_unbounded_idle_select<T: BlockingTxTrait<i32>, R: AsyncRxTrait<i32
                 pin_mut!(f);
                 select! {
                     _ = f => {
-                        let (_tx_wakers, _rx_wakers) = rx.as_ref().get_wakers_count();
-                        debug!("waker tx {} rx {}", _tx_wakers, _rx_wakers);
+                        let (tx_wakers, rx_wakers) = rx.as_ref().get_wakers_count();
+                        debug!("waker tx {} rx {}", tx_wakers, rx_wakers);
+                        assert_eq!(tx_wakers, 0, "tx wakers {}", tx_wakers);
+                        assert!(rx_wakers <= 1, "rx wakers {}", rx_wakers);
                     },
                     _ = c => {
                         unreachable!()