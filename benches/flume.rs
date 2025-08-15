f3757ff9373dabd45c529a7d63a666031505c9b9
plan <frostyplanet@gmail.com>
2025-08-15 23:18:54 +0800

    bench: Add more bench for flume

--- benches/flume.rs
+++ benches/flume.rs
@@ -60,6 +60,57 @@ fn _flume_bounded_sync(bound: usize, tx_count: usize, rx_count: usize, msg_count
     assert!(recv_counter.load(Ordering::Acquire) >= msg_count);
 }
 
+fn _flume_unbounded_sync(tx_count: usize, rx_count: usize, msg_count: usize) {
+    let (tx, rx) = flume::unbounded();
+    let counter = Arc::new(AtomicUsize::new(0));
+    let mut th_s = Vec::new();
+    for _tx_i in 0..tx_count {
+        let _counter = counter.clone();
+        let _tx = tx.clone();
+        th_s.push(thread::spawn(move || loop {
+            let i = _counter.fetch_add(1, Ordering::SeqCst);
+            if i < msg_count {
+                if let Err(e) = _tx.send(i) {
+                    panic!("send error: {:?}", e);
+                }
+            } else {
+                break;
+            }
+        }));
+    }
+    drop(tx);
+    let recv_counter = Arc::new(AtomicUsize::new(0));
+    for _ in 0..(rx_count - 1) {
+        let _rx = rx.clone();
+        let _recv_counter = recv_counter.clone();
+        th_s.push(thread::spawn(move || loop {
+            match _rx.recv() {
+                Ok(_) => {
+                    let _ = _recv_counter.fetch_add(1, Ordering::SeqCst);
+                }
+                Err(_) => {
+                    break;
+                }
+            }
+        }));
+    }
+    loop {
+        match rx.recv() {
+            Ok(_) => {
+                let _ = recv_counter.fetch_add(1, Ordering::SeqCst);
+            }
+            Err(_) => {
+                break;
+            }
+        }
+    }
+    for th in th_s {
+        let _ = th.join();
+    }
+    assert!(counter.load(Ordering::Acquire) >= msg_count);
+    assert!(recv_counter.load(Ordering::Acquire) >= msg_count);
+}
+
 async fn _flume_unbounded_async(tx_count: usize, rx_count: usize, msg_count: usize) {
     let (tx, rx) = flume::unbounded();
     let counter = Arc::new(AtomicUsize::new(0));
@@ -177,17 +228,26 @@ fn bench_flume_bounded_sync(c: &mut Criterion) {
     group.measurement_time(Duration::from_secs(15));
     for input in [1, 2, 4, 8, 16] {
         let param = Concurrency { tx_count: input, rx_count: 1 };
+        group.throughput(Throughput::Elements(TEN_THOUSAND as u64));
+        group.bench_with_input(BenchmarkId::new("mpsc size 1", input), &param, |b, i| {
+            b.iter(|| _flume_bounded_sync(1, i.tx_count, i.rx_count, TEN_THOUSAND))
+        });
+    }
+    for input in [1, 2, 4, 8, 16] {
+        let param = Concurrency { tx_count: input, rx_count: 1 };
         group.throughput(Throughput::Elements(ONE_MILLION as u64));
-        group.bench_with_input(BenchmarkId::new("mpsc", input), &param, |b, i| {
+        group.bench_with_input(BenchmarkId::new("mpsc size 100", input), &param, |b, i| {
             b.iter(|| _flume_bounded_sync(100, i.tx_count, i.rx_count, ONE_MILLION))
         });
     }
     for input in [(2, 2), (4, 4), (8, 8), (16, 16)] {
         let param = Concurrency { tx_count: input.0, rx_count: input.1 };
         group.throughput(Throughput::Elements(ONE_MILLION as u64));
-        group.bench_with_input(BenchmarkId::new("mpmc", param.to_string()), &param, |b, i| {
-            b.iter(|| _flume_bounded_sync(100, i.tx_count, i.rx_count, ONE_MILLION))
-        });
+        group.bench_with_input(
+            BenchmarkId::new("mpmc size 100", param.to_string()),
+            &param,
+            |b, i| b.iter(|| _flume_bounded_sync(100, i.tx_count, i.rx_count, ONE_MILLION)),
+        );
     }
     group.finish();
 }
@@ -245,10 +305,31 @@ fn bench_flume_bounded_async(c: &mut Criterion) {
     }
 }
 
+fn bench_flume_unbounded_sync(c: &mut Criterion) {
+    let mut group = c.benchmark_group("flume_unbounded_sync");
+    group.significance_level(0.1).sample_size(50);
+    group.measurement_time(Duration::from_secs(20));
+    for input in [(1, 1), (2, 1), (4, 1), (8, 1), (16, 1)] {
+        let param = Concurrency { tx_count: input.0, rx_count: input.1 };
+        group.throughput(Throughput::Elements(ONE_MILLION as u64));
+        group.bench_with_input(BenchmarkId::new("mpsc unbounded", &param), &param, |b, i| {
+            b.iter(|| _flume_unbounded_sync(i.tx_count, i.rx_count, ONE_MILLION))
+        });
+    }
+    for input in [(2, 2), (4, 4), (8, 8), (16, 16)] {
+        let param = Concurrency { tx_count: input.0, rx_count: input.1 };
+        group.throughput(Throughput::Elements(ONE_MILLION as u64));
+        group.bench_with_input(BenchmarkId::new("mpmc unbounded", &param), &param, |b, i| {
+            b.iter(|| _flume_unbounded_sync(i.tx_count, i.rx_count, ONE_MILLION))
+        });
+    }
+}
+
 criterion_group!(
     benches,
     bench_flume_bounded_sync,
     bench_flume_bounded_async,
     bench_flume_unbounded_async,
+    bench_flume_unbounded_sync,
 );
 criterion_main!(benches);
