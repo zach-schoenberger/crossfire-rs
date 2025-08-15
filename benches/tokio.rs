8f34c73ce1713fcc77dcddd1f7eac6124d41bdfc
plan <frostyplanet@gmail.com>
2025-08-15 19:04:57 +0800

    bench: Add unbounded for tokio

--- benches/tokio.rs
+++ benches/tokio.rs
@@ -36,8 +36,36 @@ async fn _tokio_bounded_mpsc(bound: usize, tx_count: usize, msg_count: usize) {
     }
 }
 
-fn bench_tokio(c: &mut Criterion) {
-    let mut group = c.benchmark_group("tokio_mpsc");
+async fn _tokio_unbounded_mpsc(tx_count: usize, msg_count: usize) {
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<usize>();
+
+    let counter = Arc::new(AtomicUsize::new(0));
+    for _tx_i in 0..tx_count {
+        let _tx = tx.clone();
+        let _counter = counter.clone();
+        tokio::spawn(async move {
+            loop {
+                let i = _counter.fetch_add(1, Ordering::SeqCst);
+                if i < msg_count {
+                    let _ = _tx.send(i);
+                } else {
+                    break;
+                }
+            }
+        });
+    }
+    drop(tx);
+    for _ in 0..msg_count {
+        if let Some(_msg) = rx.recv().await {
+            //    println!("recv {}", _msg);
+        } else {
+            panic!("recv error");
+        }
+    }
+}
+
+fn bench_tokio_bounded(c: &mut Criterion) {
+    let mut group = c.benchmark_group("tokio_bounded");
     group.significance_level(0.1).sample_size(50);
     group.measurement_time(Duration::from_secs(10));
     for input in [1, 2, 4, 8, 16] {
@@ -47,7 +75,22 @@ fn bench_tokio(c: &mut Criterion) {
             b.to_async(get_runtime()).iter(|| _tokio_bounded_mpsc(100, i.tx_count, ONE_MILLION))
         });
     }
+    group.finish();
 }
 
-criterion_group!(benches, bench_tokio,);
+fn bench_tokio_unbounded(c: &mut Criterion) {
+    let mut group = c.benchmark_group("tokio_unbounded");
+    group.significance_level(0.1).sample_size(50);
+    group.measurement_time(Duration::from_secs(10));
+    for input in [1, 2, 4, 8, 16] {
+        let param = Concurrency { tx_count: input, rx_count: 1 };
+        group.throughput(Throughput::Elements(ONE_MILLION as u64));
+        group.bench_with_input(BenchmarkId::new("mpsc", input), &param, |b, i| {
+            b.to_async(get_runtime()).iter(|| _tokio_unbounded_mpsc(i.tx_count, ONE_MILLION))
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_tokio_bounded, bench_tokio_unbounded);
 criterion_main!(benches);
