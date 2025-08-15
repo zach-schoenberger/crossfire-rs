7f96ebdcf5d27b880bd8f1717de47817829c11e8
plan <frostyplanet@gmail.com>
2025-08-15 23:18:54 +0800

    bench: Add size 1 bench for flume

--- benches/flume.rs
+++ benches/flume.rs
@@ -177,17 +177,26 @@ fn bench_flume_bounded_sync(c: &mut Criterion) {
     group.measurement_time(Duration::from_secs(15));
     for input in [1, 2, 4, 8, 16] {
         let param = Concurrency { tx_count: input, rx_count: 1 };
+        group.throughput(Throughput::Elements(TEN_THOUSAND as u64));
+        group.bench_with_input(BenchmarkId::new("mpsc size 1", input), &param, |b, i| {
+            b.iter(|| _flume_bounded_sync(1, i.tx_count, i.rx_count, TEN_THOUSAND))
+        });
+    }
+    for input in [1, 2, 4, 8, 16] {
+        let param = Concurrency { tx_count: input, rx_count: 1 };
         group.throughput(Throughput::Elements(ONE_MILLION as u64));
-        group.bench_with_input(BenchmarkId::new("mpsc", input), &param, |b, i| {
+        group.bench_with_input(BenchmarkId::new("mpsc size 100", input), &param, |b, i| {
             b.iter(|| _flume_bounded_sync(100, i.tx_count, i.rx_count, ONE_MILLION))
         });
     }
     for input in [(2, 2), (4, 4), (8, 8), (16, 16)] {
         let param = Concurrency { tx_count: input.0, rx_count: input.1 };
         group.throughput(Throughput::Elements(ONE_MILLION as u64));
-        group.bench_with_input(BenchmarkId::new("mpmc", param.to_string()), &param, |b, i| {
-            b.iter(|| _flume_bounded_sync(100, i.tx_count, i.rx_count, ONE_MILLION))
-        });
+        group.bench_with_input(
+            BenchmarkId::new("mpmc size 100", param.to_string()),
+            &param,
+            |b, i| b.iter(|| _flume_bounded_sync(100, i.tx_count, i.rx_count, ONE_MILLION)),
+        );
     }
     group.finish();
 }
