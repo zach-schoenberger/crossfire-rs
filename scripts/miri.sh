a4d91d0ac4e923a9dfb42fb70fb07dc2d96eba03
plan <frostyplanet@gmail.com>
2025-09-06 06:04:02 +0800

    miri.sh: pass --features trace_log as option
    Ignore parking_lot pointer case warning
    
    and add RUST_BACKTRACE
    
    Add tokio_unstable flag
    
    allow passing MIRIFLAGS from outside

--- scripts/miri.sh
+++ scripts/miri.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
 # -Zmiri-no-short-fd-operations is to prevent short write perform by miri, which breaks to atomic appending in log
-MIRIFLAGS="-Zmiri-disable-isolation -Zmiri-no-short-fd-operations -Zmiri-backtrace=full" cargo +nightly miri test $@ --features trace_log -- --no-capture --test-threads=1
+# -Zmiri-permissive-provenance is to disable warning about parking_lot
+
+# By default log is off, if you need to enable, pass the option with the script: --features trace_log
+
+MIRIFLAGS="$MIRIFLAGS -Zmiri-disable-isolation -Zmiri-no-short-fd-operations -Zmiri-backtrace=full -Zmiri-permissive-provenance"
+export MIRIFLAGS
+RUSTFLAGS="--cfg tokio_unstable" RUST_BACKTRACE=1 cargo +nightly miri test $@ -- --no-capture --test-threads=1
