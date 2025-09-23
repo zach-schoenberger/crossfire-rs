98f085c8570791e4f13dd59aa44942a431429a93
plan <frostyplanet@gmail.com>
2025-09-23 23:37:07 +0800

    miri.sh: Randomize miri seed

--- scripts/miri.sh
+++ scripts/miri.sh
@@ -4,6 +4,13 @@
 
 # By default log is off, if you need to enable, pass the option with the script: --features trace_log
 
-MIRIFLAGS="$MIRIFLAGS -Zmiri-disable-isolation -Zmiri-no-short-fd-operations -Zmiri-backtrace=full -Zmiri-permissive-provenance"
+if [ -z "$MIRI_SEED" ]; then
+	MIRI_SEED="$(shuf -i 1-1000 -n 1)"
+fi
+echo "MIRI_SEED" $MIRI_SEED
+
+MIRIFLAGS="$MIRIFLAGS -Zmiri-seed=$MIRI_SEED -Zmiri-disable-isolation -Zmiri-no-short-fd-operations -Zmiri-backtrace=full -Zmiri-permissive-provenance"
 export MIRIFLAGS
+echo $MIRIFLAGS
+
 RUSTFLAGS="--cfg tokio_unstable" RUST_BACKTRACE=1 cargo +nightly miri test $@ -- --no-capture --test-threads=1
